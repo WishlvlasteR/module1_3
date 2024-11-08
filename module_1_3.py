98092b629b510a47b90b0c117c5dbd65cdf87132
Ruslan <wishiviaster99@gmail.com>
2024-11-08 13:38:55 +1200

    08.11.24

---
+++ module_1_3.py
@@ -0,0 +1,30 @@
+                                          ##Практическое задание Модуль 1_3
+
+Name="Ruslan"
+print(Name)
+Age=35
+print(Age)
+Age=Age+1
+print(Age)
+is_student=True
+print(is_student)
+
+                                          ##Практическое задание Модуль 1_6
+
+Number_Performer=12
+Number_Hourse=1.5
+Course_Name="Python"
+Time_Per_task=Number_Hourse/Number_Performer
+print("Курс:" ,Course_Name, "," "Всего задач:" ,Number_Performer, "," "затрачено часов:" ,Number_Hourse,
+      "," "среднее время выполнения:",Time_Per_task,"часов")
+                                          ##Практическое задание Модуль 1_9
+
+example=str("Dg7jfd5kgj8hg5hs3")
+print(example[0])                     #Выведен первый символ переменной : example.
+print(example[-1])                    #Выведен последний символ переменной : example (используя отрицательный индекс).
+print(example[10:])                   #Выведена вторая половина переменной : example (С нечётным количеством символов).
+print(example[::-1])                  #Выведена на экран переменная : example на оборот.
+print(example[0::2])                  #каждый второй символ переменной : example
+
+
+
